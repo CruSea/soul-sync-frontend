60b00182a520a23e84d94f2ef6df6ccde8e027c1
Nathnael Desta <destanathnaelataro@gmail.com>
2024-11-18 00:27:17 +0300

    feat: :sparkles: Add chat messages

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1,11 +1,66 @@
+"use client"
+
+import { useState } from 'react';
 import ChatHeader from './chatHeader'
+import Today from './today'
+import Message from './message';
+
+// type for the group of message threads between user and mentor
+interface threadType {
+  isUser: boolean;
+  text: string
+  time: string
+  newDay: string
+}
 
 const Chat = () => {
+
+  const [thread, setThread] = useState<threadType[]>([
+    {
+      isUser: true,
+      text: "Hi, How are you?",
+      time: '2:43 PM',
+      newDay: 'November 14, 2024'
+    },
+    {
+      isUser: false,
+      text: "Hi Jennie, I am fine what about you?",
+      time: '2:43 PM',
+      newDay: ''
+    },
+    {
+      isUser: true,
+      text: "Lorem ipsum odor? todor? huodor hjhkhask aamet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames tempus taciti pellentesque amet amet primis neque mattis. Dictum volutpat fames tempus taciti.",
+      time: '2:44 PM',
+      newDay: ''
+      
+    },
+    {
+      isUser: false,
+      text: "Lorem ipsum odor amet amet amet amet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames ues wei ccr tempus taciti pellentesque amet amet. Aenean facilisis lacus mi interdum lacinia proin.",
+      time: '2:44 PM',
+      newDay: ''
+    },
+    {
+      isUser: true,
+      text: "Lorem ipsum odor amet amet amet amet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames ues wei ccr tempus taciti pellentesque amet amet. Aenean facilisis lacus mi interdum lacinia proin.",
+      time: '2:44 PM',
+      newDay: 'November 15, 2024'
+    }
+  ])
+
+
+
+
+
   return (
     <div className="flex-1 flex flex-col bg-white rounded-lg">
       <ChatHeader />
-      <div className='h-full w-full flex-1'>
+      <div className='relative h-full w-full flex-1 pt-12 px-4 pb-4'>
 
+        <div className="flex flex-col gap-6 w-full">
+        {thread.map((message, index) => <Message text={message.text} isUser={message.isUser} time={message.time} newDay={message.newDay} index={index}/> )}
+        </div>
       </div>
     </div>
   )
---
+++ app/components/message.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { cn } from '../utils'
+
+interface MessageProps {
+  text: string
+  isUser: boolean
+  time: string
+  newDay: string
+  index: number
+}
+
+const Message: React.FC<MessageProps> = ({ text, isUser, time, newDay, index }) => {
+  return (
+    <div className={cn("relative flex gap-2 items-end", !isUser ? "ml-auto flex-row-reverse" : "flex", newDay !== "" ? "pt-7" : '', index === 0 && newDay !== "" ? "mt-[-22px]" : '')}>
+      {/* if it is a user the account icon will be on the right side instead of left.
+          if it is the first message of the day, give a padding for the Date text.
+          if it is the first message of the day and the first message of the message thread give an extra padding for the text at top 
+      
+      */}
+      {newDay !== "" && <div className='absolute top-[-4px] left-1/2 transform translate-x-[-50%] font-normal text-xs text-neutral-400'>
+        {newDay}
+      </div>}
+      <Image
+        src="/assets/chatProfile.png"
+        alt="user image"
+        layout="fixed"
+        width={100}
+        height={100}
+        className="w-[35px] h-[35px] border-black rounded-full"
+      />
+      <div className="bg-neutral-200 text-sm font-normal p-2.5 rounded-xl max-w-[420px]">
+        {text}
+      </div>
+      <div className={cn("absolute bottom-[-15px] font-normal text-[9px] text-neutral-400", isUser ? "left-[53px]" : "right-[53px]")}>
+        {/* make the time text directly under the text */}
+        {time}
+      </div>
+    </div>
+  )
+}
+
+export default Message;
