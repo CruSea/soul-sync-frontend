e348a9937d1903ceea2d383e7684e58bdb90e542
Nathnael Desta <destanathnaelataro@gmail.com>
2024-11-18 01:01:44 +0300

    feat: :sparkles: starting search bar

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 import ChatHeader from './chatHeader'
 import ChatCss from '../styles/chat.module.css'
 import Message from './message';
+import Image from "next/image";
+import { v4 as uuidv4 } from 'uuid';
+
 
 // type for the group of message threads between user and mentor
 interface threadType {
@@ -11,6 +14,7 @@ interface threadType {
   text: string
   time: string
   newDay: string
+  id: string
 }
 
 const Chat = () => {
@@ -20,32 +24,37 @@ const Chat = () => {
       isUser: true,
       text: "Hi, How are you?",
       time: '2:43 PM',
-      newDay: 'November 14, 2024'
+      newDay: 'November 14, 2024',
+      id: uuidv4()
     },
     {
       isUser: false,
       text: "Hi Jennie, I am fine what about you?",
       time: '2:43 PM',
-      newDay: ''
+      newDay: '',
+      id: uuidv4()
     },
     {
       isUser: true,
       text: "Lorem ipsum odor? todor? huodor hjhkhask aamet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames tempus taciti pellentesque amet amet primis neque mattis. Dictum volutpat fames tempus taciti.",
       time: '2:44 PM',
-      newDay: ''
+      newDay: '',
+      id: uuidv4()
 
     },
     {
       isUser: false,
       text: "Lorem ipsum odor amet amet amet amet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames ues wei ccr tempus taciti pellentesque amet amet. Aenean facilisis lacus mi interdum lacinia proin.",
       time: '2:44 PM',
-      newDay: ''
+      newDay: '',
+      id: uuidv4()
     },
     {
       isUser: true,
       text: "Lorem ipsum odor amet amet amet amet, consectetuer adipiscing elit. Sed eros nullam fermentum viverra ante amet justo. Facilisi justo bibendum mus ante augue sem. Malesuada natoque urna ornare primis neque mattis. Dictum volutpat fames ues wei ccr tempus taciti pellentesque amet amet. Aenean facilisis lacus mi interdum lacinia proin.",
       time: '2:44 PM',
-      newDay: 'November 15, 2024'
+      newDay: 'November 15, 2024',
+      id: uuidv4()
     }
   ])
 
@@ -58,10 +67,18 @@ const Chat = () => {
       <ChatHeader />
       <div className='relative h-full w-full flex-1 pt-12 px-6 pb-4'>
         <div className={`flex-1 flex flex-col gap-6 w-full overflow-y-auto z-10 ${ChatCss.messageThread}`}>
-          {thread.map((message, index) => <Message text={message.text} isUser={message.isUser} time={message.time} newDay={message.newDay} index={index} />)}
+          {thread.map((message, index) => <Message key={message.id} text={message.text} isUser={message.isUser} time={message.time} newDay={message.newDay} index={index} />)}
         </div>
-        <div>
-          
+        <div className='relative h-[50px] mt-5 w-full rounded-[20px] bg-neutral-200 flex align-center'>
+
+          <Image
+            src="/assets/send.png"
+            alt="user image"
+            layout="fixed"
+            width={100}
+            height={100}
+            className="absolute top-1/2 transform translate-y-[-50%] right-[8px] cursor-pointer w-[35px] h-[35px] border-black rounded-full"
+          />
         </div>
       </div>
     </div>
