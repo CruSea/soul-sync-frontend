c85e8c207d07f8a6a7bde9f76479511b147808bb
Nathnael Desta <destanathnaelataro@gmail.com>
2024-11-18 15:21:58 +0300

    fix: :bug: undo font bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Sidebar from "./components/sidebar";
 import Header from "./components/header";
 import Main from "./components/main";
 
+
 export default function Home() {
   return (
-    <div className="flex font-title">
+    <div className={` flex font-title`}>
       <Sidebar />
       <div className="flex flex-col w-full max-h-screen">
-      <Header />
-      <Main />
+        <Header />
+        <Main />
       </div>
     </div>
   );
