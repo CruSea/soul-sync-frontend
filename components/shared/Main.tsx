e6d2de46ba337b095cedbd4b873ff8fb99b54e6d
Nathnael Desta <destanathnaelataro@gmail.com>
2024-11-21 08:37:17 +0300

    feat: Add layout for the main component

---
+++ components/shared/Main.tsx
@@ -0,0 +1,11 @@
+const Main = () => {
+  return (
+    <div className="flex p-5 gap-5 w-full h-full bg-gray-100">
+      <div className="w-96 h-full bg-white"></div>
+      <div className="flex-1 h-full bg-white"></div>
+      <div className="w-96 h-full bg-white"></div>
+    </div>
+  )
+}
+
+export default Main
--- components/views/mentor.tsx
+++ components/views/mentor.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/shared/Header'
+import Main from '@/components/shared/Main'
 
 const MentorView = () => {
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col w-screen h-screen">
       <Header />
+      <Main />
     </div>
 
   )
