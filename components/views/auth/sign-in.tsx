2e829626eb864be7ad5aabb0b569faaa148535ea
Nathnael Desta <destanathnaelataro@gmail.com>
2024-11-22 10:08:53 +0300

    feat: Show tokens on sign-in page for backend

--- components/views/auth/sign-in.tsx
+++ components/views/auth/sign-in.tsx
@@ -8,10 +8,16 @@ const SignInView = () => {
 
   const { data: session } = useSession();
 
+  // Access the Google tokens
+  const accessToken = session?.access_token;
+  const idToken = session?.id_token;
+
   return (
     <div>
       {session ? (
         <div><h1>Welcome back</h1>
+              {accessToken && <p>Access Token: {accessToken}</p>}
+              {idToken && <p>ID Token: {idToken}</p>}
         <Button onClick={() => signOut()}>Sign out</Button></div>
       ) : (
         <div>
