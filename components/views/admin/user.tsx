12f6f9a30d19e125c11944ca6986274deea8251b
betigirma <beti.3170.bassist@gmail.com>
2024-11-26 00:59:51 +0300

    StatCard added.

--- components/views/admin/user.tsx
+++ components/views/admin/user.tsx
@@ -1,3 +1,4 @@
+import { StatsCards } from "@/components/shared/dashboard/StatCard";
 import Header from "@/components/shared/Header";
 import Sidebar from "@/components/shared/Sidebar";
 
@@ -8,6 +9,13 @@ const AdminView = () => {
       <div className="flex flex-1">
         <div className="hidden md:block w-[300px] bg-gray-100 dark:bg-gray-800">
           <Sidebar />
+        </div>
+        <div className="flex-1 p-4 bg-secondary dark:bg-gray-900">
+          <div className="flex flex-col lg:flex-row gap-6">
+            <div className="flex-1 bg-white dark:bg-black p-16 rounded-lg">
+              <StatsCards />
+            </div>
+          </div>
         </div>
       </div>
     </div>
