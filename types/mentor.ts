4bc860753a54c4e913e0e5a32dd79e17e2289c2a
betigirma <beti.3170.bassist@gmail.com>
2024-12-02 14:32:00 +0300

    I added the mentors board in the admin

---
+++ types/mentor.ts
@@ -0,0 +1,14 @@
+export interface Mentor {
+    id: string
+    name: string
+    age: number
+    gender: string
+    email: string
+    phoneNumber: string
+    specialization: string
+    location: string
+    status: string
+    profileImage: string
+  }
+  
+  
