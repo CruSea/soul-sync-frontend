3a6461a62a88307a3d8b0e582f816ad60d1c659b
Nathnael Desta <destanathnaelataro@gmail.com>
2024-12-10 20:52:48 +0300

    chore:

---
+++ components/views/mentor/get-started.tsx
@@ -0,0 +1,12 @@
+import LoginPageCard from "@/components/shared/LoginPage/LoginPageCard"
+import LoginPageSidebar from "@/components/shared/LoginPage/LoginPageSidebar"
+
+const GetStartedView = () => {
+  return (
+    <div className="w-screen h-screen flex">
+
+    </div>
+  )
+}
+
+export default GetStartedView
