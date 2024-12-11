036983b07af5d98d304c90784b049202886441ee
Nathnael Desta <destanathnaelataro@gmail.com>
2024-12-11 13:43:51 +0300

    feat: Add a routing to the backend login

--- components/shared/LoginPage/LoginPageCard.tsx
+++ components/shared/LoginPage/LoginPageCard.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -7,10 +9,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useRouter } from "next/navigation"
 import Link from "next/link"
 
 
 const LoginPageCard = () => {
+  const router = useRouter();
+
+
+  const handleLogin = () => {
+    router.push("https://jlh2d981-3000.uks1.devtunnels.ms/auth/google")
+  };
+
 
   return (
     <Card className="flex-1 flex items-center justify-center flex-col gap-6">
@@ -23,13 +33,7 @@ const LoginPageCard = () => {
         </CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-4 pt-0">
-        <Button className="w-96 h-10 font-medium text-sm" variant="outline">Login with Google</Button>
-        <div className="text-sm text-center">Don’t have an Organization Platform?</div>
-        <Link href="/create-org" >
-          <Button className="w-96 h-10 font-medium text-sm" variant="default">
-            Sign Up
-          </Button>
-        </Link>
+        <Button className="w-96 h-10 font-medium text-sm" variant="default" onClick={handleLogin}>Login with Google</Button>
       </CardContent>
     </Card>
   )
