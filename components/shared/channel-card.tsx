0588d50763a262d9ddac7f3c03ea9f4291309acf
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2024-12-13 11:37:54 +0300

    feat: Add functionality of Delete Button
    The Functionality of Delete Button was added

--- components/shared/channel-card.tsx
+++ components/shared/channel-card.tsx
@@ -4,9 +4,10 @@ import { AiOutlineDelete } from "react-icons/ai";
 
 interface ChannelCardProps {
   channel: Channel;
+  setChannel: React.Dispatch<React.SetStateAction<Channel[]>>;
 }
 
-export function ChannelCard({ channel }: ChannelCardProps) {
+export function ChannelCard({ channel, setChannel }: ChannelCardProps) {
   let iconURL = "";
   switch (channel.type) {
     case "Telegram Bot":
@@ -28,6 +29,11 @@ export function ChannelCard({ channel }: ChannelCardProps) {
       iconURL = "";
       break;
   }
+
+  const handleDeleteChannel = (id:string) => {
+    setChannel((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="h-[278px] w-full  flex flex-col items-center justify-between px-2.5 pt-1 pb-2 border rounded-xl bg-white hover:shadow-md hover:rounded-xl transition-shadow">
       <div className="h-10 px-1 py-2 rounded-tl-lg rounded-tr-lg justify-between items-center inline-flex w-full">
@@ -36,8 +42,12 @@ export function ChannelCard({ channel }: ChannelCardProps) {
             Active
           </div>
         </div>
-        {/* <div className="w-6 h-6 px-[3px] py-[2.62px] justify-center items-center flex"></div> */}
-        <AiOutlineDelete className="w-5 h-5 cursor-pointer hover:bg-[#f1f2f4] rounded-xl" />
+        <div
+          onClick={() => handleDeleteChannel(channel.id)}
+          className="w-6 h-6 px-[3px] py-[2.62px] justify-center rounded-xl hover:bg-[#f1f2f4] items-center cursor-pointer flex"
+        >
+          <AiOutlineDelete className="w-full h-full cursor-pointer  " />
+        </div>
       </div>
       <div className="relative w-16 h-16 mb-2">
         <Image
