4bc02859a3d469b98e5d0ddfa80caf7133828db9
Nathnael Desta <destanathnaelataro@gmail.com>
2024-12-15 11:57:31 +0300

    feat: Add a database to fetch teh users list data from
    Tried to fetch from api/mentor/users.ts but for some reason i get 404 no page found on all the api routes

--- components/views/mentor/mentor-chat.tsx
+++ components/views/mentor/mentor-chat.tsx
@@ -2,7 +2,16 @@ import Chat from "@/components/shared/Mentor/Chat";
 import Profile from "@/components/shared/Mentor/Profile";
 import Search from "@/components/shared/Mentor/Search";
 
-const MentorView = () => {
+const MentorView = async () => {
+  // Fetch users from the JSON Server
+  const response = await fetch("http://localhost:3001/users");
+  if (!response.ok) {
+    throw new Error("Failed to fetch users from JSON Server");
+  }
+
+  const users = await response.json();
+
+  console.log("users list ", users)
   return (
     <>
       <Search />
