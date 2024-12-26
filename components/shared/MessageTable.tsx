44e4f5767a381df216413af7ecfd544b9ddae4d3
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2024-12-26 11:56:35 +0300

    chores: move "message" data to its own component

--- components/shared/MessageTable.tsx
+++ components/shared/MessageTable.tsx
@@ -6,19 +6,7 @@ import { MessageCircle } from "lucide-react";
 import type { Message } from "@/types/message";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Column, FilterOption } from "@/types/data-table";
-
-const MESSAGE_DATA: Message[] = Array.from({ length: 350 }, (_, i) => ({
-  id: `message-${i + 1}`,
-  mentorName: `Mentor ${i + 1}`,
-  menteeName: `Mentee ${i + 1}`,
-  mentorProfileImage: `/mentor-${i + 1}.svg?height=40&width=40`,
-  menteeProfileImage: `/mentee-${i + 1}.svg?height=40&width=40`,
-  startDate: "16/09/2024",
-  startTime: "02:00PM",
-  status: i < 5 ? "On Going" : "Ended",
-  endDate: i < 5 ? "N/A" : "16/09/2024",
-  endTime: i < 5 ? "N/A" : "10:00PM",
-}));
+import { MESSAGE_DATA } from "@/data/message_data";
 
 const columns: Array<Column<Message>> = [
   {
