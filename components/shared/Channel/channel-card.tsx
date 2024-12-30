ed70d06c761d9b38ec85c1b445a2996020b01131
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2024-12-30 23:05:09 +0300

    fix: Eslint Error

--- components/shared/Channel/channel-card.tsx
+++ components/shared/Channel/channel-card.tsx
@@ -26,13 +26,13 @@ interface ChannelCardProps {
 
 export function ChannelCard({ channel, setChannels, toast }: ChannelCardProps) {
   const [deleteId, setDeleteId] = useState<string | null>(null);
-  let iconURL = "";
+  let iconURL = '';
   switch (channel.Metadata.type) {
-    case "Telegram Bot":
-      iconURL = "/telegram.png";
+    case 'Telegram Bot':
+      iconURL = '/telegram.png';
       break;
-    case "Negarit SMS":
-      iconURL = "/negarit.png";
+    case 'Negarit SMS':
+      iconURL = '/negarit.png';
       break;
     case 'WhatsApp':
       iconURL = '/Whatsapp.png';
@@ -57,23 +57,23 @@ export function ChannelCard({ channel, setChannels, toast }: ChannelCardProps) {
         prevItems.filter((item) => item.id !== deleteId)
       );
       fetch(`http://localhost:3001/channels/${deleteId}`, {
-        method: "DELETE",
+        method: 'DELETE',
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log("Deleted channel:", data);
+          console.log('Deleted channel:', data);
           // Show toast notification after successful deletion
           toast({
-            title: "Channel deleted successfully",
-            description: "The channel has been deleted from the list",
+            title: 'Channel deleted successfully',
+            description: 'The channel has been deleted from the list',
             duration: 3000,
           });
         })
         .catch((error) => {
-          console.error("Error:", error);
+          console.error('Error:', error);
           toast({
-            title: "Error deleting channel",
-            description: "An error occurred while deleting the channel.",
+            title: 'Error deleting channel',
+            description: 'An error occurred while deleting the channel.',
             duration: 3000,
           });
         });
@@ -83,7 +83,7 @@ export function ChannelCard({ channel, setChannels, toast }: ChannelCardProps) {
   };
   const channelChange = (channel: Channel) => {
     switch (channel.Metadata.type) {
-      case "Telegram Bot":
+      case 'Telegram Bot':
         return (
           <div className="self-stretch justify-between items-center inline-flex gap-9">
             <div className="h-[19px] justify-start items-center gap-2 flex">
@@ -97,7 +97,7 @@ export function ChannelCard({ channel, setChannels, toast }: ChannelCardProps) {
             </div>
           </div>
         );
-      case "Negarit SMS":
+      case 'Negarit SMS':
         return (
           <div>
             <div className="self-stretch justify-between items-center inline-flex gap-9">
@@ -200,7 +200,7 @@ export function ChannelCard({ channel, setChannels, toast }: ChannelCardProps) {
               Are you sure you want to delete?{' '}
               <span className="font-bold text-black inline">
                 {channel.name} | {channel.Metadata.type}
-              </span>{" "}
+              </span>{' '}
               action cannot be undone.
             </DialogDescription>
           </DialogHeader>
--- types/channel.ts
+++ types/channel.ts
@@ -1,11 +1,11 @@
-import * as z from "zod";
+import * as z from 'zod';
 
 export type ChannelType =
-  | "Telegram Bot"
-  | "WhatsApp"
-  | "Negarit SMS"
-  | "Facebook"
-  | "Twilio";
+  | 'Telegram Bot'
+  | 'WhatsApp'
+  | 'Negarit SMS'
+  | 'Facebook'
+  | 'Twilio';
 
 export interface Channel {
   id: string;
@@ -25,41 +25,41 @@ export interface Channel {
 export const formSchema = z
   .object({
     name: z.string().min(2, {
-      message: "Channel name must be at least 2 characters.",
+      message: 'Channel name must be at least 2 characters.',
     }),
     type: z.enum([
-      "Telegram Bot",
-      "WhatsApp",
-      "Negarit SMS",
-      "Facebook",
-      "Twilio",
+      'Telegram Bot',
+      'WhatsApp',
+      'Negarit SMS',
+      'Facebook',
+      'Twilio',
     ]),
     apiKey: z.string().optional(),
     token: z.string().optional(),
     campaignId: z.string().optional(),
   })
   .superRefine((channel, ctx) => {
-    if (channel.type === "Telegram Bot" && !channel.apiKey) {
+    if (channel.type === 'Telegram Bot' && !channel.apiKey) {
       ctx.addIssue({
-        code: "custom",
-        path: ["apiKey"],
-        message: "API Key is required for Telegram Bot.",
+        code: 'custom',
+        path: ['apiKey'],
+        message: 'API Key is required for Telegram Bot.',
       });
     }
 
-    if (channel.type === "Negarit SMS") {
+    if (channel.type === 'Negarit SMS') {
       if (!channel.token) {
         ctx.addIssue({
-          code: "custom",
-          path: ["token"],
-          message: "Token is required for Negarit SMS.",
+          code: 'custom',
+          path: ['token'],
+          message: 'Token is required for Negarit SMS.',
         });
       }
       if (!channel.campaignId) {
         ctx.addIssue({
-          code: "custom",
-          path: ["campaignId"],
-          message: "Campaign ID is required for Negarit SMS.",
+          code: 'custom',
+          path: ['campaignId'],
+          message: 'Campaign ID is required for Negarit SMS.',
         });
       }
     }
