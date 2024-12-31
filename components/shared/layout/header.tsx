8acf214b54b27c441e5f0711f39e4545d54a26b3
Nathnael Desta <destanathnaelataro@gmail.com>
2024-12-31 06:15:04 +0300

    feat: Make a dialog appear when clicking account icon

--- components/shared/layout/header.tsx
+++ components/shared/layout/header.tsx
@@ -1,5 +1,15 @@
 import { FiBell } from "react-icons/fi";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./accountDialog";
+import { LuUser } from "react-icons/lu";
+import { LuLogOut } from "react-icons/lu";
 
 export function Header({ title }: HeaderProps) {
   return (
@@ -9,15 +19,31 @@ export function Header({ title }: HeaderProps) {
         <button className="text-gray-600 hover:text-gray-900">
           <FiBell className="h-5 w-5" />
         </button>
-        <Avatar className="w-8 h-8 cursor-pointer">
-            <AvatarImage
-              src="/assets/avatars/woman1.png"
-              className="w-full h-full object-cover"
-            />
-            <AvatarFallback className="w-full h-full flex items-center justify-center text-xl">
-              CN
-            </AvatarFallback>
-          </Avatar>
+        <Dialog>
+          <DialogTrigger>
+            <Avatar className="w-8 h-8 cursor-pointer">
+              <AvatarImage
+                src="/assets/avatars/woman1.png"
+                className="w-full h-full object-cover"
+              />
+              <AvatarFallback className="w-full h-full flex items-center justify-center text-xl">
+                CN
+              </AvatarFallback>
+            </Avatar>
+          </DialogTrigger>
+          <DialogContent className="rounded-[6px] pt-4 pb-0 px-0 w-min translate-x-0 translate-y-0 top-12 left-auto right-2.5 ">
+            <DialogHeader className="space-y-0 px-4">
+              <DialogTitle className="text-start text-lg">Nathnael Desta</DialogTitle>
+              <DialogDescription className="text-base">
+                nathnaeldesta@gmail.com
+              </DialogDescription>
+            </DialogHeader>
+            <div className="flex flex-col">
+              <div className="flex gap-2 text-lg py-2 border border-y-slate-200 border-x-0 px-4 cursor-pointer hover:bg-slate-100"><LuUser size={25} /> Profile</div>
+              <div className="flex gap-2 text-lg py-2 px-4 cursor-pointer hover:bg-slate-100"><LuLogOut size={25}/> Logout</div>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </header>
   );
