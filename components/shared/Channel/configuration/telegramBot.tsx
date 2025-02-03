f50fdb07c16353b0c033690665133600b005e9d1
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-02-03 16:47:14 +0300

    fix: adjust alignment of items in telegramBot component

--- components/shared/Channel/configuration/telegramBot.tsx
+++ components/shared/Channel/configuration/telegramBot.tsx
@@ -7,7 +7,7 @@ type telegramProp = {
 function telegramBot({ channel }: telegramProp) {
   const config = channel.configuration as telegramConfig;
   return (
-    <div className="w-full justify-between items-center flex ">
+    <div className="w-full justify-between items-start flex ">
       <div className="h-auto w-fit gap-2 items-center flex  justify-between">
         <div className="w-2.5 h-2.5 bg-[#27a376] rounded-[50px]" />
         <div className="text-[#677488] text-xs font-medium font-['Manrope'] leading-tight">
