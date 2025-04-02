af2566dd6004440913fef912e407c9bdb2c2918f
betigirma <beti.3170.bassist@gmail.com>
2025-04-02 08:41:17 -0300

    added invitedAdmins endpoint

--- data/end-points.ts
+++ data/end-points.ts
@@ -8,6 +8,7 @@ export const endPoints = {
   allConversations: 'conversation',
   channel: 'admin/channel',
   message: 'admin/messages',
+  invitedAdmins: 'admin/user/all'
 };
 
 export const jsonServer = {
