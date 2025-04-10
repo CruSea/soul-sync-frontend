8c2a252f12e746fe8b3e4168cd4ce80a4827b538
betigirma <beti.3170.bassist@gmail.com>
2025-04-10 15:19:03 -0300

    Removed the duplicate SocketProvider from mentor layout since it's now at the root.

--- components/shared/layout/mentor-layout.tsx
+++ components/shared/layout/mentor-layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from '@/components/shared/layout/header';
-import SocketProvider from '@/context/providers/SocketProvider';
 import { MentorLayoutProps } from '@/types/mentor';
 
 export default function MentorLayout({ children, title }: MentorLayoutProps) {
@@ -8,7 +7,7 @@ export default function MentorLayout({ children, title }: MentorLayoutProps) {
     <div className="flex flex-1 flex-col h-screen">
       <Header title={title} />
       <div className="flex-1 flex p-5 gap-5 w-full overflow-hidden bg-gray-100">
-        <SocketProvider>{children}</SocketProvider>
+        {children}
       </div>
     </div>
   );
